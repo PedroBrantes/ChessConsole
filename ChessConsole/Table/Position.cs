6cb0b713fc93532abc3d572047a54cb5e491f7fc
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2024-06-11 19:27:21 -0300

    Update Class Position

--- ChessConsole/Table/Position.cs
+++ ChessConsole/Table/Position.cs
@@ -2,20 +2,20 @@
 {
     internal class Position
     {
-        public int Line { get; set; }
-        public int Column { get; set; }
+        public int line { get; set; }
+        public int column { get; set; }
 
         public Position(int line, int column)
         {
-            this.Line = line;
-            this.Column = column;
+            this.line = line;
+            this.column = column;
         }
 
         public override string ToString()
         {
-            return Line
+            return line
                 + ", "
-                + Column;
+                + column;
         }
     }
 }
