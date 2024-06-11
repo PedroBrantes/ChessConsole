a3f5de8cdf776dab5ce3b75687fd750df0cb2b66
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2024-06-11 19:28:55 -0300

    Created Class Table in Table

---
+++ ChessConsole/Table/Table.cs
@@ -0,0 +1,17 @@
+﻿namespace ChessConsole.Table
+{
+    internal class Table
+    {
+        public int lines { get; set; }
+        public int columns { get; set; }
+
+        private Piece[,] piece;
+
+        public Table(int lines, int columns)
+        {
+            this.lines = lines;
+            this.columns = columns;
+            piece = new Piece[lines,columns];
+        }
+    }
+}
