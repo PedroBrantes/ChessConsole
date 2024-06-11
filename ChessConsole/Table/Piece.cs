85adcbda6fca0e2eda462da3ab825737000b644e
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2024-06-11 19:28:31 -0300

    Created Class Piece in Table

---
+++ ChessConsole/Table/Piece.cs
@@ -0,0 +1,21 @@
+﻿namespace ChessConsole.Table
+{
+    internal class Piece
+    {
+        public Position position { get; set; }
+        public Color color { get; protected set; }
+
+        public int quantityMoves { get; protected set; }
+
+        public Table table { get; protected set; }
+
+        public Piece(Position position, Table table, Color color)
+        {
+            this.position = position;
+            this.table = table;
+            this.color = color;
+            this.quantityMoves = 0;
+        }
+
+    }
+}
