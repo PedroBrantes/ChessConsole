50aab1323db492200511d2ded47191a4972fc489
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2024-06-11 19:00:29 -0300

    New Folder Table & Class Position

--- ChessConsole/Program.cs
+++ ChessConsole/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Chess!");
+﻿using System;
+using ChessConsole.Table;
+
+namespace ChessConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position P;
+
+            P = new Position(2, 4);
+
+            Console.WriteLine("Position: " + P);
+            Console.ReadLine();
+        }
+    }
+}
